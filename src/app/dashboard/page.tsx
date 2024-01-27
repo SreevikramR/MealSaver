bc24d49d091c03270fb00a063a777acbcdad0997
SreevikramR <sreevikram.r@gmail.com>
2024-01-27 15:12:53 -0600

    feat: dashboard UI added

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const _card = () => {
+    return (
+        <div className='mt-4 ml-10 w-5/6 border-2 border-black rounded-xl p-3 flex flex-row'>
+            <div className='w-3/4 flex flex-col'>
+                <div>ABC Club</div>
+                <div>Free Pizza and Drinks</div>
+                <div>ILCB 100</div>
+            </div>
+            <div className='text-center justify-end flex content-center flex-wrap'>
+                <div>7:00 PM</div>
+                <div>1st February 2024</div>
+            </div>
+        </div>
+    );
+}
+
+const page = () => {
+    return (
+        <div>
+            <div className='bg-black text-white p-4 text-3xl font-medium tracking-wider w-screen fixed'>
+                MealSaver
+            </div>
+            <div className='flex flex-row ml-4 pt-16'>
+                <div className='w-1/2'>
+                    <div className='ml-10 pt-10 mb-8 flex flex-row border-b-2 border-black w-fit'>
+                        <div className='text-2xl pb-2 '>Clubs and Organizations</div>
+                        <div className='text-2xl ml-5 border-b-4 border-black'>Restaurants</div>
+                    </div>
+
+                    <_card/>
+                    <_card/>
+                    <_card/>
+                    <_card/>
+                    <_card/>
+                    <_card/>
+                    <_card/>
+                </div>
+                <div className='h-[80vh] w-1/2 flex flex-wrap align-middle mt-10 mr-6 fixed right-0'>
+                    <div className='border-2 border-black h-full w-full'></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default page
