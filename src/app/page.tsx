21b7a25acf12a7f992403e9cdc9b59b4c139c4f9
Meet Gamdha <gamdhameet@gmail.com>
2024-01-27 16:59:29 -0600

    ButtonLanding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,17 @@
 
 export default function Home() {
     return (
+      
 		<div>
 			<div className="bg-gray-100 flex flex-col justify-center items-center h-screen">
 				<div className="text-5xl font-bold text-gray-800 mt-16">Meal Saver</div>
 				<div className="text-lg text-gray-600 mb-16">Savor Savings, Satisfy Cravings: Your Ultimate Food Coupon and Free Food Finder</div>
-			</div>
+        
+        <button className="bg-red-500 hover:bg-red text-white font-bold py-2 px-4 rounded-full ">Login</button>
+        <button className="bg-red-500 hover:bg-red text-white font-bold py-2 px-4 rounded-full">Sign Up</button>
+			
+      
+</div>
 		</div>
     );
 }
