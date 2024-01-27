3c9b6674a39637cd7c858cc40a38b229e03ec365
Meet Gamdha <gamdhameet@gmail.com>
2024-01-27 17:18:56 -0600

    Linkedpages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
 			<div className="bg-gray-100 flex flex-col justify-center items-center h-screen">
 				<div className="text-5xl font-bold text-gray-800 mt-16">Meal Saver</div>
 				<div className="text-lg text-gray-600 mb-16">Savor Savings, Satisfy Cravings: Your Ultimate Food Coupon and Free Food Finder</div>
-        
-        <button className="bg-red-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-full ">Login</button>
-        <button className="bg-red-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-full">Sign Up</button>
-			
+        <a href="login"> <button className="bg-red-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-full " >Login</button>
+</a>
+        <a href="signup"><button className="bg-red-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-full">Sign Up</button>
+      </a>
       
 </div>
 		</div>
