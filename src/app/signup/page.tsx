bc0d48587dadb1347427ecd55f65c69dc02c1f96
SreevikramR <sreevikram.r@gmail.com>
2024-01-27 13:51:12 -0600

    feat: sign up page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -9,7 +9,7 @@ const page = () => {
     return (
         <div className='w-screen h-screen flex justify-center items-center flex-col'>
             <div className='w-1/3 h-4/5'>
-                <div className='pt-2 font-semibold text-3xl text-center'>Login</div>
+                <div className='pt-2 font-semibold text-3xl text-center'>Sign Up</div>
                 <div className='flex justify-center items-center flex-col text-xl pt-3'>
                     <div className='w-3/4'>
                         <div className='text-2xl'>Email</div>
@@ -20,7 +20,7 @@ const page = () => {
                         <input className='w-full border-2 border-black rounded-lg p-2' type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <div className='w-3/4 pt-10'>
-                        <button className='w-full border-2 border-black bg-black text-white p-2 rounded-lg'>Login</button>
+                        <button className='w-full border-2 border-black bg-black text-white p-2 rounded-lg'>Sign Up</button>
                     </div>
                 </div>
             </div>
