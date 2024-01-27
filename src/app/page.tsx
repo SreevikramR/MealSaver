add33a3ba1f072f413640db2c220d0ad2c44f326
Meet Gamdha <gamdhameet@gmail.com>
2024-01-27 17:05:41 -0600

    improve button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
 				<div className="text-5xl font-bold text-gray-800 mt-16">Meal Saver</div>
 				<div className="text-lg text-gray-600 mb-16">Savor Savings, Satisfy Cravings: Your Ultimate Food Coupon and Free Food Finder</div>
         
-        <button className="bg-red-500 hover:bg-red text-white font-bold py-2 px-4 rounded-full ">Login</button>
-        <button className="bg-red-500 hover:bg-red text-white font-bold py-2 px-4 rounded-full">Sign Up</button>
+        <button className="bg-red-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-full ">Login</button>
+        <button className="bg-red-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-full">Sign Up</button>
 			
       
 </div>
