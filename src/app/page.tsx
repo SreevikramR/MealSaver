e8e54efb92030d639fed15dbb8825c1289ff21b9
SreevikramR <sreevikram.r@gmail.com>
2024-01-27 13:42:14 -0600

    fix: hydration errors landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,12 @@
-import Image from "next/image";
+"use client"
 
 export default function Home() {
-  return (
-    
-    <head>
-    {/* <meta charset="UTF-8"> */}
-    {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"> */}
-    <title>Meal Saver</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"/>
-
-<body className="bg-gray-100 flex flex-col justify-center items-center h-screen">
-    <h1 className="text-5xl font-bold text-gray-800 mt-16">Meal Saver</h1>
-    <h2 className="text-lg text-gray-600 mb-16">Savor Savings, Satisfy Cravings: Your Ultimate Food Coupon and Free Food Finder</h2>
-</body>
-</head>
-    
-  );
+    return (
+		<div>
+			<div className="bg-gray-100 flex flex-col justify-center items-center h-screen">
+				<div className="text-5xl font-bold text-gray-800 mt-16">Meal Saver</div>
+				<div className="text-lg text-gray-600 mb-16">Savor Savings, Satisfy Cravings: Your Ultimate Food Coupon and Free Food Finder</div>
+			</div>
+		</div>
+    );
 }
