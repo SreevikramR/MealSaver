433a9f4e82acb85ed61fe3ce411b7788f13d8b34
Meet Gamdha <gamdhameet@gmail.com>
2024-01-27 21:23:40 -0600

    Made buttons bigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
 				<div className="text-5xl font-bold text-gray-800 mt-16">Meal Saver</div>
 				<div className="text-lg text-gray-600 mb-16">Savor Savings, Satisfy Cravings: Your Ultimate Food Coupon and Free Food Finder</div>
         
-      <div><a href="login flex-row"> <button className="bg-black hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-full " >Login</button>
+      <div className="flex flex-row "><a href="login" className="flex-row pr-10"> <button className="bg-black hover:bg-gray-600 text-white font-bold py-4 px-7 rounded-full " >Login</button>
 </a>
-        <a href="signup"><button className="bg-black hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-full">Sign Up</button>
+        <a href="signup"><button className="bg-black hover:bg-gray-600 text-white font-bold py-4 px-7 rounded-full">Sign Up</button>
       </a></div>
-</div>
+</div> 
 		</div>
     );
 }
