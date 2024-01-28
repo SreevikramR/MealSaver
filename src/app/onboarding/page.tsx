0e2abd423777a1e7c6a6b6acda2631b7f0f90773
SreevikramR <sreevikram.r@gmail.com>
2024-01-27 22:04:09 -0600

    feat: onboarding page UI added

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,49 @@
+"use client"
+import React, { useState } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { useRouter } from 'next/navigation'
+import Image from 'next/image'
+import saveMoneyImg from '../../../public/save_money.png'
+import restaurantImg from '../../../public/restaurant.jpg'
+import clubImg from '../../../public/club.png'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY || ''
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+const Page = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const router = useRouter()
+
+    return (
+        <div className='w-screen h-screen flex mt-24 items-center flex-col'>
+            <div className='w-4/5 h-2/5'>
+                <div className='pt-2 font-semibold text-3xl text-center mb-10'>What brings you to MealSaver?</div>
+                <div className='flex justify-center items-center flex-row text-xl pt-3 h-full'>
+                    <div className='w-3/4 border-2 border-slate-300 text-center mr-8 h-full rounded-xl flex flex-wrap flex-col justify-center place-items-center hover:border-black hover:cursor-pointer'>
+                        <div className='flex relative w-2/5 justify-center mb-8'>
+                            <Image src={saveMoneyImg} alt='Save Money' layout='contain'/>
+                        </div>
+                        <div className='font-semibold text-2xl h-1/6'>Just want to save some money!</div>
+                    </div>
+                    <div className='w-3/4 border-2 border-slate-300 text-center mr-8 h-full rounded-xl flex flex-wrap flex-col justify-center place-items-center hover:border-black hover:cursor-pointer'>
+                        <div className='flex relative w-2/5 justify-center mb-8'>
+                            <Image src={clubImg} alt='Save Money' layout='contain'/>
+                        </div>
+                        <div className='font-semibold text-2xl h-1/6'>Bring more people to my Club / Organization Events</div>
+                    </div>
+                    <div className='w-3/4 border-2 border-slate-300 text-center mr-8 h-full rounded-xl flex flex-wrap flex-col justify-center place-items-center hover:border-black hover:cursor-pointer'>
+                        <div className='flex relative w-2/5 justify-center mb-8'>
+                            <Image src={restaurantImg} alt='Save Money' layout='contain'/>
+                        </div>
+                        <div className='font-semibold text-2xl h-1/6'>Promote my Restaurant</div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Page
